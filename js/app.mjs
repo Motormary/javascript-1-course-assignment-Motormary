88ef35abae018a205f9309013251d71b9bca4769
Mathias <mat.moen@gmail.com>
2024-01-18 16:59:23 +0100

    first commit

---
+++ js/app.mjs
@@ -0,0 +1,9 @@
+async function getAllProducts() {
+  const response = await fetch("https://api.noroff.dev/api/v1/rainy-days", {
+    method: "GET",
+  });
+  const products = await response.json();
+  console.log(products);
+}
+
+getAllProducts()
