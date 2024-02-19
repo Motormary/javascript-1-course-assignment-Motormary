0c8f3d7e4b2d67dda3c1c43e307c46b39f3f377f
Motormary <mat.moen@gmail.com>
2024-02-19 09:28:29 +0100

    clean

--- js/functions/toast.mjs
+++ js/functions/toast.mjs
@@ -1,10 +1,9 @@
 export function showToast(message, href = "/", duration = 5000) {
   const toast = document.getElementById("toast");
-  toast.style = ""
   toast.href = href
   toast.innerText = message;
   toast.classList.add("show");
-  setTimeout(function () {
+  setTimeout(() => {
     toast.classList.remove("show");
   }, duration);
 }
